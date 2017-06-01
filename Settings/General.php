9c6f45f5761136e02d438ca95ac80c19afcf0cfe
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-01 18:25:03 +0300

    no message

---
+++ Settings/General.php
@@ -0,0 +1,20 @@
+<?php
+namespace Df\Oro\Settings;
+// 2017-06-01
+/** @method static General s() */
+final class General extends \Df\Config\Settings {
+	/**
+	 * 2017-06-01
+	 * @return string
+	 */
+	function key() {return $this->p();}
+
+	/**
+	 * 2017-06-01
+	 * @override
+	 * @see \Df\Config\Settings::prefix()
+	 * @used-by \Df\Config\Settings::v()
+	 * @return string
+	 */
+	protected function prefix() {return 'df_oro/general';}
+}
--- T/Basic.php
+++ T/Basic.php
@@ -1,7 +1,8 @@
 <?php
 namespace Df\Oro\T;
+use Df\Oro\Settings\General as S;
 // 2017-06-01
 final class Basic extends TestCase {
 	/** @test 2017-06-01 */
-	function t01() {echo __METHOD__;}
+	function t01() {echo S::s()->key();}
 }
