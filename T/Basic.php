19d619f29e2bae8374d83ea4f23c22c0e2cc8490
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-01 17:59:26 +0300

    no message

---
+++ T/Basic.php
@@ -0,0 +1,7 @@
+<?php
+namespace Df\Oro\T;
+// 2017-06-01
+final class Basic extends TestCase {
+	/** @test 2017-06-01 */
+	function t01() {echo __METHOD__;}
+}
---
+++ T/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+namespace Df\Oro\T;
+/**
+ * 2017-06-01
+ * @see \Df\Oro\T\Basic
+ */
+abstract class TestCase extends \Df\Core\TestCase {}
