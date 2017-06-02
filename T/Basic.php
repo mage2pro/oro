9eb38e9cc1afd3d559074ec5e28c4dfcf25196ed
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-02 20:14:54 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -1,8 +1,20 @@
 <?php
 namespace Df\Oro\T;
-use Df\Oro\Settings\General as S;
+use Zend_Http_Client as C;
 // 2017-06-01
 final class Basic extends TestCase {
 	/** @test 2017-06-01 */
-	function t01() {echo S::s()->key();}
+	function t01() {
+		/** @var C $c */
+		$c = (new C)
+			->setConfig(['timeout' => 120])
+			->setHeaders(['content-type' => 'application/json'] + df_oro_headers())
+			->setMethod(C::GET)
+			->setUri("https://localhost.com:848/app_dev.php/api/rest/latest/users")
+		;
+		$c->setAdapter((new \Zend_Http_Client_Adapter_Socket)->setStreamContext([
+			'ssl' => ['allow_self_signed' => true, 'verify_peer' => false]
+		]));
+		echo df_dump(df_json_decode($c->request()->getBody()));
+	}
 }
--- lib/main.php
+++ lib/main.php
@@ -1,2 +1,27 @@
 <?php
-function df_oro() {}
+use Df\Oro\Settings\General as S;
+/**
+ * 2017-06-02
+ * «How is «oro:wsse:generate-header» implemented?» https://oplatform.club/t/84
+ * @param string|null $username [optional]
+ * @param string|null $key [optional]
+ * @return array(string => string)
+ */
+function df_oro_headers($username = null, $key = null) {
+	/** @var string $created */
+	$created = date('c');
+	/** @var string $nonce */
+	$nonce = base64_encode(substr(md5(uniqid(gethostname() . '_', true)), 0, 16));
+	return [
+		'Authorization' => 'WSSE profile="UsernameToken"'
+		,'X-WSSE' => 'UsernameToken ' . df_csv_pretty(df_map_k(function($k, $v) {return
+		"$k=\"$v\""
+	;}, [
+		'Username' => $username ?: S::s()->username()
+		,'PasswordDigest' => base64_encode(sha1(implode([
+			base64_decode($nonce), $created, $key ?: S::s()->key()
+		]), true))
+		,'Nonce' => $nonce
+		,'Created' => $created
+	]))];
+}
