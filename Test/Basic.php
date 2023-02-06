fcccdd5027c875c4ea151e345a9bafe845fcadae
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-06 07:50:42 +0000

    https://github.com/mage2pro/core/issues/174

--- Test/Basic.php
+++ Test/Basic.php
@@ -7,7 +7,7 @@ final class Basic extends TestCase {
 	 * «How to include related entities to a response on a Web API «get list» request?»
 	 * https://oplatform.club/t/105
 	 */
-	function t01_orders() {print_r(df_json_encode(df_oro_get_list(
+	function t01_orders():void {print_r(df_json_encode(df_oro_get_list(
 		'orders', ['product' => 1], ['product', 'website'], true
 	)));}
 
